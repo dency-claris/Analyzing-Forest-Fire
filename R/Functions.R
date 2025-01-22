dafab13ff50469b88ac503a7f282411b6b0c9b20
dency-claris <dencyclaris@gmail.com>
2025-01-23 00:06:59 +0100

    Removing outliers by filtering the area to be less than 300 while plotting the pointplot

--- R/Functions.R
+++ R/Functions.R
@@ -48,13 +48,16 @@ plot_boxplot <- function(data){
 }
 
 plot_pointplot <- function(data){
-  data |>
+  data %>%
+    filter(area < 300) %>%
     ggplot(aes(x = value, y = area)) +
     geom_point() +
     facet_wrap(vars(data_col), scales = "free_x") +
     labs(
-      title = "Relationships between other variables and area burned",
+      title = "Relationships between other variables and area burned (area < 300)",
       x = "Value of column",
       y = "Area burned (hectare)"
     )
 }
+
+
