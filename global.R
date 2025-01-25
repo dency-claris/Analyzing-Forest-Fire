5916a8b879b79cfa8eaba9066e26e6a75783413c
dency-claris <dencyclaris@gmail.com>
2025-01-25 15:56:22 +0100

    Created and updated the global.R file

--- global.R
+++ global.R
@@ -1,13 +1,19 @@
 library(tidyverse)
 library(targets)
+library(ggradar)
+
 cat("The 'targets' package has been successfully loaded.\n")
 
 
-source("Functions.R")
+source("R/Functions.R")
 data("forest_fires")
 
 # Make sure you ran tar_make() so forest_fires is available in your _targets/ cache
 forest_fires <- tar_read(forest_fires)
+forest_fires_long <- tar_read(forest_fires_long)
 
+# Helper vectors for dropdowns
+all_months <- sort(unique(forest_fires$month))
+all_days <- sort(unique(forest_fires$day))
 
 enableBookmarking(store = "url")
